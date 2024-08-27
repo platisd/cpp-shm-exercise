d4b769cf61678acba9e61ae840684f4ff8eb7e24
AeroStun <24841307+AeroStun@users.noreply.github.com>
2024-08-27 14:49:24 +0000

    Uplift C++ standard and deps' versions
    Use the latest and greatest in this exercise

--- example/data_creator_writer_shm.cpp
+++ example/data_creator_writer_shm.cpp
@@ -2,6 +2,7 @@
 #include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
 #include <thread>
